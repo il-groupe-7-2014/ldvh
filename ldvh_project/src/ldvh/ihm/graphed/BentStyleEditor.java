c4e222d1813520e7d71668b893c5b9853e8b4f19
Stéphane <meresse>
2014-12-04 20:27:51 +0100

    maj

---
+++ ldvh_project/src/ldvh/ihm/graphed/BentStyleEditor.java
@@ -0,0 +1,30 @@
+package ldvh.ihm.graphed;
+
+/**
+   A property editor for the BentStyle type.
+*/
+public class BentStyleEditor extends PropertySelector
+{
+   public BentStyleEditor()
+   {
+      super(names, values);
+   }
+
+   private static final String[] names = 
+   { 
+      "Straight", 
+      "HV", 
+      "VH", 
+      "HVH", 
+      "VHV" 
+   };
+   
+   private static final Object[] values = 
+   {
+      BentStyle.STRAIGHT,
+      BentStyle.HV,
+      BentStyle.VH,
+      BentStyle.HVH,
+      BentStyle.VHV
+   };
+}
---
+++ ldvh_project/src/ldvh/livre/Livre.java
@@ -0,0 +1,25 @@
+package ldvh.livre;
+/**
+ * 
+ */
+
+/** 
+ * <!-- begin-UML-doc -->
+ * <!-- end-UML-doc -->
+ * @author 3403266
+ * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+ */
+public class Livre {
+	/** 
+	 * <!-- begin-UML-doc -->
+	 * <!-- end-UML-doc -->
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private String titre;
+	/** 
+	 * <!-- begin-UML-doc -->
+	 * <!-- end-UML-doc -->
+	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private String auteur;
+}
